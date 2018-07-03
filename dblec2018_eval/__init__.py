7c91b5e7a97cd2dec0530374ea8c143ca3ed2434
Kazutoshi Umemoto <umemoto@tkl.iis.u-tokyo.ac.jp>
2018-07-03 18:53:32 +0900

    Add package metadata

---
+++ dblec2018_eval/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+__name__ = 'dblec2018_eval'
+__description__ = 'A codebase for task-2 of database engineering 2018'
+__author__ = 'Kazutoshi Umemoto'
+__email__ = 'umemoto@tkl.iis.u-tokyo.ac.jp'
+__version__ = '0.0.0'
